32ad9e1ac24e81bb5d9302e835ac0fe843dc4235
Maicon Moraes <maicon3000@hotmail.com>
2023-03-12 19:32:20 -0300

    Criação do menu principal - https://matrixy.atlassian.net/browse/MATRIXY-9

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:matrixy/hidden_drawer.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,20 +12,26 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      home: const HiddenDrawer(),
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+        primarySwatch: const MaterialColor(
+          0xFF804300,
+          <int, Color>{
+            50: Color(0xFFFFF3E0),
+            100: Color(0xFFFFE0B2),
+            200: Color(0xFFFFCC80),
+            300: Color(0xFFFFB74D),
+            400: Color(0xFFFFA726),
+            500: Color(0xFF804300),
+            600: Color(0xFF6E3B00),
+            700: Color(0xFF5D3200),
+            800: Color(0xFF4E2900),
+            900: Color(0xFF3E1F00),
+          },
+
+        ),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
